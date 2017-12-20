e48d614110921b66449f55c958e1394654ab14b7
Masimba Gomba Ndadzibaya <masimban@sahomeloans.com>
2017-12-20 15:06:28 +0200

    Complete chapter one

---
+++ ConsoleApp1/ConsoleApp1/DuckTypes/RubberDuck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ConsoleApp1.QuackBehaviour;
+
+namespace ConsoleApp1.DuckTypes
+{
+	class RubberDuck : Duck
+	{
+		public RubberDuck() : base(new MuteQuack(), new FlyNoWay())
+		{
+			
+
+		}
+
+		public override void Display()
+		{
+			Console.WriteLine("whatever");
+		}
+	}
+}
