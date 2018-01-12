954ff51cadc5b0a1b2f832df0faf50541b71e8fa
Masimba Gomba Ndadzibaya <masimban@sahomeloans.com>
2018-01-12 08:06:41 +0200

    Developed the Factory pattern ConsoleApp1 - CHAPTER FOUR (4).

---
+++ Chapter_4_Factory_Pattern/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	public interface Shape
+	{
+		void draw();
+	}
+
+	public class Circle : Shape
+	{
+		public void draw()
+		{
+			Console.WriteLine("I am the CIRCLE Please draw me:  ");
+		}
+	}
+
+	public class Rectangle : Shape
+	{
+		public void draw()
+		{
+			Console.WriteLine("I am the RECTANGLE Please draw me:  ");
+		}
+	}
+
+	public class Square : Shape
+	{
+		public void draw()
+		{
+			Console.WriteLine("I am the SQUARE Please draw me:  ");
+		}
+	}
+
+	public class ShapeFactory
+	{
+		public Shape GetShapeType(String shapeType)
+		{
+			if (shapeType == null)
+			{
+				return null;
+			}
+			if (shapeType.Equals("circle"))
+			{
+				return new Circle();
+			}
+			if (shapeType.Equals("rectangle"))
+			{
+				return new Rectangle();
+			}
+			if (shapeType.Equals("square"))
+			{
+				return new Square();
+			}
+			return null;
+		}
+	}
+
+	
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ShapeFactory shapeFactory = new ShapeFactory();
+
+			Shape shape1 = shapeFactory.GetShapeType("circle");
+			shape1.draw();
+
+			Shape shape2 = shapeFactory.GetShapeType("rectangle");
+			shape2.draw();
+
+			Shape shape3 = shapeFactory.GetShapeType("square");
+			shape3.draw();
+
+			Console.ReadLine();
+		}
+	}
+}
